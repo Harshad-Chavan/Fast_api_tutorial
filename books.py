ac612512324c96a5edb5748fb9e0755bbba9599c
harshad <harshadchavan141@gmail.com>
2023-03-18 18:30:39 +0530

    Day1:Enhance api endpoint

--- books.py
+++ books.py
@@ -2,6 +2,15 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/api-endpoint")
-def first_api():
-    return {'message':'Hello Harshad'}
+BOOKS = [
+    {'title': "Title1","author": "Author 1","category": "Science"},
+    {'title': "Title2","author": "Author 2","category": "Science"},
+    {'title': "Title3","author": "Author 3","category": "History"},
+    {'title': "Title4","author": "Author 4","category": "Math"},
+    {'title': "Title5","author": "Author 5","category": "Math"},
+    {'title': "Title6","author": "Author 6","category": "Math"},
+]
+
+@app.get("/books")
+async def read_all_books():
+    return BOOKS
