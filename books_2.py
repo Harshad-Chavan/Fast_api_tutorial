8723fe233afd1a5e7e689d2fa9c26daaa32d8185
harshad <harshadchavan141@gmail.com>
2023-04-07 15:44:47 +0530

    Day5: added explicit status codes using starlette

--- books_2.py
+++ books_2.py
@@ -4,6 +4,7 @@ from typing import Optional
 # use Query to add vlidation to Query paramets
 from fastapi import FastAPI, Path, Query, HTTPException
 from pydantic import BaseModel, Field
+from starlette import status
 
 app = FastAPI()
 
@@ -60,13 +61,13 @@ BOOKS = [
 ]
 
 
-@app.get("/books")
+@app.get("/books",status_code=status.HTTP_200_OK)
 async def read_all_books():
     return BOOKS
 
 
 # adding extrra validation using Path
-@app.get("/books/{book_id}")
+@app.get("/books/{book_id}",status_code=status.HTTP_200_OK)
 async def get_book_by_id(book_id: int = Path(gt=0)):
     for book in BOOKS:
         if book.id == book_id:
@@ -74,8 +75,7 @@ async def get_book_by_id(book_id: int = Path(gt=0)):
     raise HTTPException(status_code=404, detail="item not found")
 
 
-
-@app.get("/books/publish/")
+@app.get("/books/publish/",status_code=status.HTTP_200_OK)
 async def get_book_by_publish_date(published_date: int = Query(gt=1999, lt=2031)):
     books_returned = []
     for book in BOOKS:
@@ -84,7 +84,7 @@ async def get_book_by_publish_date(published_date: int = Query(gt=1999, lt=2031)
     return books_returned
 
 
-@app.get("/books/")
+@app.get("/books/",status_code=status.HTTP_200_OK)
 async def get_book_by_rating(book_rating: int = Query(gt=0, lt=6)):
     books_returned = []
     for book in BOOKS:
@@ -95,13 +95,13 @@ async def get_book_by_rating(book_rating: int = Query(gt=0, lt=6)):
 
 # pydantic is used for data modelling,data parsing and efficient error handling
 # we are ensuring the the post req is having approprite data before transforming into book object
-@app.post("/books/create_book")
+@app.post("/books/create_book",status_code=status.HTTP_201_CREATED)
 async def create_book(book_request: BookRequest):
     new_book = Book(**book_request.dict())
     BOOKS.append(find_book_id(new_book))
 
 
-@app.put("/books/update_book")
+@app.put("/books/update_book",status_code=status.HTTP_204_NO_CONTENT)
 async def update_book(book_request: BookRequest):
     book_changed = False
     for i in range(len(BOOKS)):
@@ -109,8 +109,7 @@ async def update_book(book_request: BookRequest):
             BOOKS[i] = Book(**book_request.dict())
             book_changed = True
     if not book_changed:
-        raise HTTPException(status_code=404 , detail="item not found")
-
+        raise HTTPException(status_code=404, detail="item not found")
 
 
 @app.delete("/books/{book_id}")
@@ -122,7 +121,7 @@ async def delete_book(book_id: int = Path(gt=0)):
             book_changed = True
             break
     if not book_changed:
-        raise HTTPException(status_code=404 , detail="item not found")
+        raise HTTPException(status_code=404, detail="item not found")
 
 
 # creating this function to get the max id in the list and assign it to the book
