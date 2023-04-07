d2d050b8d52198941d0a93d01d125a15f356f7b0
harshad <harshadchavan141@gmail.com>
2023-04-07 16:33:27 +0530

    Day5: added explicit status codes using starlette

--- books_2.py
+++ books_2.py
@@ -61,13 +61,13 @@ BOOKS = [
 ]
 
 
-@app.get("/books",status_code=status.HTTP_200_OK)
+@app.get("/books", status_code=status.HTTP_200_OK)
 async def read_all_books():
     return BOOKS
 
 
 # adding extrra validation using Path
-@app.get("/books/{book_id}",status_code=status.HTTP_200_OK)
+@app.get("/books/{book_id}", status_code=status.HTTP_200_OK)
 async def get_book_by_id(book_id: int = Path(gt=0)):
     for book in BOOKS:
         if book.id == book_id:
@@ -75,7 +75,7 @@ async def get_book_by_id(book_id: int = Path(gt=0)):
     raise HTTPException(status_code=404, detail="item not found")
 
 
-@app.get("/books/publish/",status_code=status.HTTP_200_OK)
+@app.get("/books/publish/", status_code=status.HTTP_200_OK)
 async def get_book_by_publish_date(published_date: int = Query(gt=1999, lt=2031)):
     books_returned = []
     for book in BOOKS:
@@ -84,7 +84,7 @@ async def get_book_by_publish_date(published_date: int = Query(gt=1999, lt=2031)
     return books_returned
 
 
-@app.get("/books/",status_code=status.HTTP_200_OK)
+@app.get("/books/", status_code=status.HTTP_200_OK)
 async def get_book_by_rating(book_rating: int = Query(gt=0, lt=6)):
     books_returned = []
     for book in BOOKS:
@@ -95,13 +95,13 @@ async def get_book_by_rating(book_rating: int = Query(gt=0, lt=6)):
 
 # pydantic is used for data modelling,data parsing and efficient error handling
 # we are ensuring the the post req is having approprite data before transforming into book object
-@app.post("/books/create_book",status_code=status.HTTP_201_CREATED)
+@app.post("/books/create_book", status_code=status.HTTP_201_CREATED)
 async def create_book(book_request: BookRequest):
     new_book = Book(**book_request.dict())
     BOOKS.append(find_book_id(new_book))
 
 
-@app.put("/books/update_book",status_code=status.HTTP_204_NO_CONTENT)
+@app.put("/books/update_book", status_code=status.HTTP_204_NO_CONTENT)
 async def update_book(book_request: BookRequest):
     book_changed = False
     for i in range(len(BOOKS)):
