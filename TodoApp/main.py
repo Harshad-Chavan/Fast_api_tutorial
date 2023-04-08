19f4142a983e7964c038d97e8afc3b4833fcdb91
harshad <harshadchavan141@gmail.com>
2023-04-08 16:14:24 +0530

    Day6: created get db function and first api to fetch all todos

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI
+# Depends stands for dependencies. when one fucntion os dependent on the
+# execution of another func we use depends
+from fastapi import FastAPI, Depends
 import models
-from database import engine
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
@@ -8,6 +11,15 @@ app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
 
 
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close
+
+
 @app.get("/")
-async def create_database():
-    return f"Database: Created"
+# this will get executed after the get_db function
+async def read_all(db: Session = Depends(get_db)):
+    return db.query(models.Todos).all()
