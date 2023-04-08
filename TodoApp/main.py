bdb9fa1f6dfbff9b64b1b181d3f938a42723e672
harshad <harshadchavan141@gmail.com>
2023-04-08 16:28:15 +0530

    Day6: fetch todo based on id else raise exception (used filter in query)

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,9 +1,10 @@
 # Depends stands for dependencies. when one fucntion os dependent on the
 # execution of another func we use depends
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 import models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
+from starlette import status
 
 app = FastAPI()
 
@@ -18,8 +19,19 @@ def get_db():
     finally:
         db.close
 
+def http_exception_404():
+    return HTTPException(status_code=404, detail="Item not found")
 
 @app.get("/")
 # this will get executed after the get_db function
 async def read_all(db: Session = Depends(get_db)):
     return db.query(models.Todos).all()
+
+
+@app.get("/todos/{todo_id}", status_code=status.HTTP_200_OK)
+async def read_todo(todo_id: int, db: Session = Depends(get_db)):
+    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+
+    if todo_model is not None:
+        return todo_model
+    raise http_exception_404()
