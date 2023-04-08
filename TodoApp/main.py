e7774db044cd11521c6f5915c87bd24bdc452857
harshad <harshadchavan141@gmail.com>
2023-04-08 17:19:50 +0530

    Day6: create update todo request

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -13,6 +13,7 @@ app = FastAPI()
 # the line creates the .db sqlite file which contains database
 models.Base.metadata.create_all(bind=engine)
 
+
 def get_db():
     try:
         db = SessionLocal()
@@ -20,15 +21,16 @@ def get_db():
     finally:
         db.close
 
+
 def http_exception_404():
     return HTTPException(status_code=404, detail="Item not found")
 
 
 class Todo(BaseModel):
-    title : str
-    description : Optional[str]
-    priority : int = Field(gt=0,lt=6,description="Priority must be between 1 & 5")
-    complete : bool
+    title: str
+    description: Optional[str]
+    priority: int = Field(gt=0, lt=6, description="Priority must be between 1 & 5")
+    complete: bool
 
 
 @app.get("/")
@@ -45,9 +47,24 @@ async def read_todo(todo_id: int, db: Session = Depends(get_db)):
         return todo_model
     raise http_exception_404()
 
-@app.post("/todos/create_todo")
-async def create_todo(todo:Todo,db: Session = Depends(get_db)):
+@app.put("/todos/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def update_todo(todo_id: int,todo: Todo, db: Session = Depends(get_db)):
+    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+    print(todo.dict())
+    if todo_model is None:
+        raise http_exception_404()
+
+    todo_model.title = todo.title
+    todo_model.description = todo.description
+    todo_model.priority = todo.priority
+    todo_model.complete = todo.complete
+    db.add(todo_model)
+    db.commit()
+
+
+@app.post("/todos/create_todo",status_code=status.HTTP_201_CREATED)
+async def create_todo(todo: Todo, db: Session = Depends(get_db)):
     todo_model = models.Todos(**todo.dict())
     db.add(todo_model)
     db.commit()
-    return {'status_code':201,"transaciton" : "Succesful"}
+
