1ceef1d035f6257bdf8f49dd1e5e5095cdd7a6d6
harshad <harshadchavan141@gmail.com>
2023-04-08 15:59:10 +0530

    Day5: created models for the todo app

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,10 +1,12 @@
-from fastapi import  FastAPI
+from fastapi import FastAPI
 import models
 from database import engine
 
 app = FastAPI()
 
+# the line creates the .db sqlite file which contains database
 models.Base.metadata.create_all(bind=engine)
+
 
 @app.get("/")
 async def create_database():
