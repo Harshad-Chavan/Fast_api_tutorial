81ba7b25f248eec06cd635792f29ffaca9ee7cea
harshad <harshadchavan141@gmail.com>
2023-05-01 18:44:42 +0530

    day7: password hasing using bcrypt and passlib

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter
 from pydantic import BaseModel, Field
 from models import User
+from passlib.context import CryptContext
 
 # this tell that this file is not an application
 # we will add these routes in the main file from where they can be reachable
 router = APIRouter()
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 class CreateUserRequest(BaseModel):
@@ -18,14 +20,15 @@ class CreateUserRequest(BaseModel):
 
 @router.post("/auth")
 async def create_user(create_user_request: CreateUserRequest):
+
     create_user_model = User(
         username=create_user_request.username,
         email=create_user_request.email,
         first_name=create_user_request.first_name,
         last_name=create_user_request.last_name,
-        hashed_password=create_user_request.password,
+        hashed_password=bcrypt_context.hash(create_user_request.password),
         role=create_user_request.role,
-        active=True
+        active=True,
     )
 
     return create_user_model
