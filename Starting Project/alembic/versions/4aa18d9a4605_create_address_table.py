76e3c9ccd26287a5e08b330024edf73d2ad83547
harshad <harshadchavan141@gmail.com>
2023-07-04 21:17:01 +0530

    day 12 : installed alembic created the config files and on test version file

---
+++ Starting Project/alembic/versions/4aa18d9a4605_create_address_table.py
@@ -0,0 +1,24 @@
+"""create address table
+
+Revision ID: 4aa18d9a4605
+Revises: 93e237e95383
+Create Date: 2023-07-04 21:15:15.430203
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4aa18d9a4605'
+down_revision = '93e237e95383'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
---
+++ Starting Project/alembic/versions/93e237e95383_create_phone_number_colum_for_user_table.py
@@ -0,0 +1,24 @@
+"""Create phone number colum for user table
+
+Revision ID: 93e237e95383
+Revises: 
+Create Date: 2023-07-04 21:01:02.602875
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '93e237e95383'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.add_column('users', sa.Column('phone number', sa.String(), nullable=True))
+
+
+def downgrade() -> None:
+    op.drop_column('users', 'phone number')
